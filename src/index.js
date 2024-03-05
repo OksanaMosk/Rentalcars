70b9cac15e7336d5564bf4e44411d3f08b04e8e6
OksanaMosk <oks-m3@ukr.net>
2024-03-05 22:06:00 +0200

    start

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="Rentalcars">
+    <BrowserRouter basename="Rentalcars1">
       <Provider store={store}>
         <PersistGate persistor={persistor}>
           <App />
