835258f568721648d952354def3261987fe6d656
OksanaMosk <oks-m3@ukr.net>
2024-03-07 20:42:44 +0200

    styles

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,27 +1,16 @@
-import { ContactElement } from '../ContactElement/ContactElement';
-import { useDispatch, useSelector } from 'react-redux';
-import React, { useState, useEffect } from 'react';
-import { fetchContacts } from 'redux/contacts/contacts.reducer';
 // import { useParams } from 'react-router-dom';
-import { selectContacts } from 'redux/contacts/contacts.selector';
 // import { selectFilterTerm } from 'redux/filter/filter.selector';
-
 // import { NavLink } from 'react-router-dom';
+
+import { ContactElement } from '../ContactElement/ContactElement';
+import { useSelector } from 'react-redux';
+import React from 'react';
+import { selectContacts } from 'redux/contacts/contacts.selector';
 
 import css from './ContactList.module.css';
 
 export const ContactList = () => {
-  const [currentPage, setCurrentPage] = useState(1);
   const listResults = useSelector(selectContacts);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchContacts({ page: currentPage, limit: 12 }));
-  }, [dispatch, currentPage]);
-
-  const handleLoadMore = () => {
-    setCurrentPage(prevPage => prevPage + 1);
-  };
 
   return (
     <>
@@ -33,9 +22,6 @@ export const ContactList = () => {
             ))}
         </ul>
       </div>
-      <button className={css.button} onClick={handleLoadMore}>
-        Load more
-      </button>
     </>
   );
 };
--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -1,20 +1,52 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import { fetchContacts } from 'redux/contacts/contacts.reducer';
+import { useSelector, useDispatch } from 'react-redux';
 import { ContactList } from 'components/ContactList/ContactList';
 import { useLocation } from 'react-router-dom';
 import { Navigate, NavLink } from 'react-router-dom';
 import Filter from 'components/Filter/Filter';
 import Loader from 'components/Loader/Loader';
 import { useRef } from 'react';
-// import error1 from 'images/icons8-no-yelling-100.png';
+
 import css from './CatalogPage.module.css';
 
 const CatalogPage = () => {
-  // const contacts = useSelector(state => state.contactsStore.contacts);
+  const dispatch = useDispatch();
+
   const isLoading = useSelector(state => state.contactsStore.isLoading);
   const error = useSelector(state => state.contactsStore.error);
   const location = useLocation();
   const backLinkRef = useRef(location.state?.from ?? '/');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const newContacts = await dispatch(
+          fetchContacts({ page: currentPage, limit: 12 })
+        );
+        if (newContacts.length === 0) {
+          setHasMore(false);
+        }
+      } catch (error) {
+        console.error('Error fetching contacts:', error);
+      }
+    };
+
+    fetchData();
+  }, [dispatch, currentPage]);
+
+  const handleLoadMore = () => {
+    if (hasMore) {
+      // Перевірка наявності нових контактів перед завантаженням
+      setCurrentPage(prevPage => prevPage + 1);
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+    }
+  };
 
   return (
     <div className={css.contactsContainer}>
@@ -29,19 +61,11 @@ const CatalogPage = () => {
       <Filter />
       <ContactList />
       {isLoading && <Loader />}
-      {/* {contacts.length !== 0 ? (
-        <>
-       
-
-          <ContactList />
-        </>
-      ) : (
-        <>
-          <p className={css.noContacts}>Your phonebook is empty.</p>
-          <p className={css.noContacts}> Add your first contact!</p>
-          <img src={error1} alt="{svgDelete}" width={200} height={200}></img>
-        </>
-      )} */}
+      {hasMore && (
+        <button className={css.button} onClick={handleLoadMore}>
+          Load more
+        </button>
+      )}
     </div>
   );
 };
